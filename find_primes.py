1514c1678562e30fb30f9a13a55c691593c946d7
Chance <cembreyfarquhar@gmail.com>
2019-08-22 20:28:37 -0500

    find_primes is functional!

--- find_primes.py
+++ find_primes.py
@@ -2,15 +2,16 @@ import sys
 user_input = int(sys.argv[1])
 
 
-def is_prime(num, prime_set):
-    for prime in prime_set:
-        if num % prime == 0:
-            return False
-    return True
+def is_prime(num):
+    if num > 1:
+        for i in range(2, num // 2):
+            if (num % i) == 0:
+                print(num, " is not prime")
+                break
+        else:
+            print(num, " is prime!!!")
+    else:
+        print(num, " is not prime.")
 
 if __name__ == "__main__":
-    primes = set({2,3})
-    for prime in primes:
-        if user_input % prime == 0:
-            break
-        print("It's prime!")
+    is_prime(user_input)
