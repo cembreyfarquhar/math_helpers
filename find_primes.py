5b780091c4f0d852d9a4b2c3329a73ba84a05031
Chance <cembreyfarquhar@gmail.com>
2019-08-22 20:15:48 -0500

    broken!

--- find_primes.py
+++ find_primes.py
@@ -1,2 +1,16 @@
+import sys
+user_input = int(sys.argv[1])
+
+
+def is_prime(num, prime_set):
+    for prime in prime_set:
+        if num % prime == 0:
+            return False
+    return True
+
 if __name__ == "__main__":
-    print("Hey!")
+    primes = set({2,3})
+    for prime in primes:
+        if user_input % prime == 0:
+            break
+        print("It's prime!")
